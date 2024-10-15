22a77ee8114afbc6f93ab6fbb9b688aad1212cf4
tad <tuan.a@hsenidmobile.com>
2024-10-15 22:38:21 +0530

    added logs final - ready to release

--- src/app/api/v0/employee/[empId]/route.ts
+++ src/app/api/v0/employee/[empId]/route.ts
@@ -1,5 +1,6 @@
 import {NextRequest, NextResponse} from "next/server";
 import prisma from "@/utils/extensions/db-client";
+import apiLogger from "@/utils/extensions/apiLogger";
 
 export async function GET(
     req: NextRequest,
@@ -9,10 +10,13 @@ export async function GET(
     try {
         const employee = await prisma.employee.findUnique({where: {id: Number(empId)}})
         if (employee){
+            apiLogger.info(`Fetched employee data: ${JSON.stringify(employee)} to db successfully!!`)
             return NextResponse.json(employee, {status: 200})
         }
+        apiLogger.info(`Employee data with empId: ${empId} does not exist!`)
         return  new Response(JSON.stringify({errMsg: `Employee Not found for Id ${empId}`}), {status: 404})
     } catch (e) {
+        apiLogger.error(`Failed to get employee data, error: ${e}!`)
         return  new Response(JSON.stringify({errMsg: `Error: ${e}`}), {status: 500})
     }
 }
@@ -25,11 +29,15 @@ export async function DELETE(
     try {
         const employee = await prisma.employee.findUnique({where: {id: Number(empId)}})
         if (employee){
+            apiLogger.info(`Fetched employee data: ${JSON.stringify(employee)} to db successfully!!`)
             await prisma.employee.delete({where: {id: Number(empId)}})
+            apiLogger.info("Deleted employee data from db successfully!!")
             return new Response(null, {status: 204})
         }
+        apiLogger.info(`Employee data with empId: ${empId} does not exist!`)
         return  new Response(JSON.stringify({errMsg: `Employee Not found for Id ${empId}`}), {status: 404})
     } catch (e) {
+        apiLogger.error(`Failed to delete employee data, error: ${e}!`)
         return  new Response(JSON.stringify({errMsg: `Error: ${e}`}), {status: 500})
     }
 }
