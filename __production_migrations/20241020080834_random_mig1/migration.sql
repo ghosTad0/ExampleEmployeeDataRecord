213bcc9cf365e4575d49f75e3f4c06d957ec7d21
tad <tuan.a@hsenidmobile.com>
2024-10-20 23:39:50 +0530

    new migrations and docker compose

---
+++ __production_migrations/20241020080834_random_mig1/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `employee` ADD COLUMN `random_val1` VARCHAR(191) NULL;
--- start_app.sh
+++ start_app.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 migration_name=init_$(cat /proc/sys/kernel/random/uuid)
-cp -r __prod_migrations/* /app/node_modules/.prisma/client/migrations   # PROPER WAY OF MIGRATING. COPY SCHEMA GENERATED IN DEV ENV, THEN RUN MIGRATE DEPLOY
+cp -r __production_migrations/* /app/node_modules/.prisma/client/migrations   # PROPER WAY OF MIGRATING. COPY SCHEMA GENERATED IN DEV ENV, THEN RUN MIGRATE DEPLOY
 # yes | npx prisma migrate dev --skip-generate --create-only --name $migration_name --schema node_modules/.prisma/client/schema.prisma   # THIS DOES NOT WORK PROPERLY IN NON-INTERACTIVE ENVS, SOON GET TO THE BEST PRACTICES
 yes | npx prisma migrate deploy --schema node_modules/.prisma/client/schema.prisma
 node server.js
